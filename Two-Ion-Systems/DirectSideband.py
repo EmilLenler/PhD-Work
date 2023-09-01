047d79cc29d10e25110c135642767e90cfc6373d
Emil Lenler-Eriksen <lenler@hys.au.dk>
2023-09-01 13:43:25 +0200

    Added some stuff to calculate max allowed rabi freq on carrier as well as the corresponding sideband rabi frequency

--- Two-Ion-Systems/DirectSideband.py
+++ Two-Ion-Systems/DirectSideband.py
@@ -79,65 +79,65 @@ ax2twin.set_ylabel(r'$\eta_i^2$ for Porypherine with 550nm light',color = 'r')
 # ax.set_xticks(np.arange(0,12)+1)
 # ax.set_xlabel('Number of porypherines')
 # ax.set_ylabel('Rabi frequency / kHz')
-# state = np.array([0,1,0,0],dtype = np.cdouble)
-# ExcState = np.array([0,0,0,1],dtype = np.cdouble)
-# dt = 1e-11
-# ts = np.arange(0,1e-4,dt)
-# hbar = 1.05*1e-34
-# state1s = np.zeros(ts.size,dtype = np.clongdouble)
-# state2s = np.zeros(ts.size,dtype = np.clongdouble)
-# state3s = np.zeros(ts.size,dtype = np.clongdouble)
-# state4s = np.zeros(ts.size,dtype = np.clongdouble)
+state = np.array([0,1,0,0],dtype = np.cdouble)
+ExcState = np.array([0,0,0,1],dtype = np.cdouble)
+dt = 1e-11
+ts = np.arange(0,1e-4,dt)
+hbar = 1.05*1e-34
+state1s = np.zeros(ts.size,dtype = np.clongdouble)
+state2s = np.zeros(ts.size,dtype = np.clongdouble)
+state3s = np.zeros(ts.size,dtype = np.clongdouble)
+state4s = np.zeros(ts.size,dtype = np.clongdouble)
 
 
 #adding 3.24/2*1e3 works at a = -0.00074
 
 
-# t_end = 1e-3
-# def TimeEv(t,phi):
-#    return 1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(2*np.pi*75*1e3,-frequencies[2]+25.3*2*np.pi/2*1e3,frequencies[2],t,m_ba,vectors[2][0],k),phi)
-# UnExcitedEvolution = solve_ivp(TimeEv,[0,t_end],state,t_eval = np.linspace(0,t_end,10000),max_step = 1e-8)
-# ExcitedEvolution = solve_ivp(TimeEv,[0,t_end],np.array([0,0,0,1],dtype = np.clongdouble),t_eval=np.linspace(0,t_end,10000),max_step = 1e-8)
-# print(UnExcitedEvolution.y)
-# fig,ax = plt.subplots(2,1,figsize = (10,15))
-# print('UneXc Evo')
-# ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[0]*np.conjugate(UnExcitedEvolution.y[0]),label = r'$\vert 0,e\rangle$')
-# ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[1]*np.conjugate(UnExcitedEvolution.y[1]),label = r'$\vert 0,g\rangle$')
-# ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[2]*np.conjugate(UnExcitedEvolution.y[2]),label = r'$\vert 1,e\rangle$')
-# ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[3]*np.conjugate(UnExcitedEvolution.y[3]),label = r'$\vert 1,g\rangle$')
-# ax[0].plot(UnExcitedEvolution.t/(1e-3),np.abs(UnExcitedEvolution.y[0])**2+np.abs(UnExcitedEvolution.y[1])**2+np.abs(UnExcitedEvolution.y[2])**2+np.abs(UnExcitedEvolution.y[3])**2,color = 'k')
-# ax[0].legend()
-# ax[0].set_title(r'Initialization in ground state $\vert 0,g\rangle$ - No recoil kick')
+t_end = 1e-3
+def TimeEv(t,phi):
+   return 1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(2*np.pi*75*1e3,-frequencies[2]+25.3*2*np.pi/2*1e3,frequencies[2],t,m_ba,vectors[2][0],k),phi)
+UnExcitedEvolution = solve_ivp(TimeEv,[0,t_end],state,t_eval = np.linspace(0,t_end,10000),max_step = 1e-8)
+ExcitedEvolution = solve_ivp(TimeEv,[0,t_end],np.array([0,0,0,1],dtype = np.clongdouble),t_eval=np.linspace(0,t_end,10000),max_step = 1e-8)
+print(UnExcitedEvolution.y)
+fig,ax = plt.subplots(2,1,figsize = (10,15))
+print('UneXc Evo')
+ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[0]*np.conjugate(UnExcitedEvolution.y[0]),label = r'$\vert 0,e\rangle$')
+ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[1]*np.conjugate(UnExcitedEvolution.y[1]),label = r'$\vert 0,g\rangle$')
+ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[2]*np.conjugate(UnExcitedEvolution.y[2]),label = r'$\vert 1,e\rangle$')
+ax[0].plot(UnExcitedEvolution.t/(1e-3),UnExcitedEvolution.y[3]*np.conjugate(UnExcitedEvolution.y[3]),label = r'$\vert 1,g\rangle$')
+ax[0].plot(UnExcitedEvolution.t/(1e-3),np.abs(UnExcitedEvolution.y[0])**2+np.abs(UnExcitedEvolution.y[1])**2+np.abs(UnExcitedEvolution.y[2])**2+np.abs(UnExcitedEvolution.y[3])**2,color = 'k')
+ax[0].legend()
+ax[0].set_title(r'Initialization in ground state $\vert 0,g\rangle$ - No recoil kick')
 # print(np.max(np.abs(np.imag(UnExcitedEvolution.y[0]*np.conjugate(UnExcitedEvolution.y[0])))))
 # print(np.max(np.abs(np.imag(UnExcitedEvolution.y[1]*np.conjugate(UnExcitedEvolution.y[1])))))
 # print(np.max(np.abs(np.imag(UnExcitedEvolution.y[2]*np.conjugate(UnExcitedEvolution.y[2])))))
 # print(np.max(np.abs(np.imag(UnExcitedEvolution.y[3]*np.conjugate(UnExcitedEvolution.y[3])))))
-# print('ExcEvo')
-# ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[0]*np.conjugate(ExcitedEvolution.y[0]),label = r'$\vert 0,e\rangle$')
-# ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[1]*np.conjugate(ExcitedEvolution.y[1]),label = r'$\vert 0,g\rangle$')
-# ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[2]*np.conjugate(ExcitedEvolution.y[2]),label = r'$\vert 1,e\rangle$')
-# ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[3]*np.conjugate(ExcitedEvolution.y[3]),label = r'$\vert 1,g\rangle$')
-# ax[1].plot(ExcitedEvolution.t/(1e-3),np.abs(ExcitedEvolution.y[0])**2+np.abs(ExcitedEvolution.y[1])**2+np.abs(ExcitedEvolution.y[2])**2+np.abs(ExcitedEvolution.y[3])**2,color = 'k')
+print('ExcEvo')
+ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[0]*np.conjugate(ExcitedEvolution.y[0]),label = r'$\vert 0,e\rangle$')
+ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[1]*np.conjugate(ExcitedEvolution.y[1]),label = r'$\vert 0,g\rangle$')
+ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[2]*np.conjugate(ExcitedEvolution.y[2]),label = r'$\vert 1,e\rangle$')
+ax[1].plot(ExcitedEvolution.t/(1e-3),ExcitedEvolution.y[3]*np.conjugate(ExcitedEvolution.y[3]),label = r'$\vert 1,g\rangle$')
+ax[1].plot(ExcitedEvolution.t/(1e-3),np.abs(ExcitedEvolution.y[0])**2+np.abs(ExcitedEvolution.y[1])**2+np.abs(ExcitedEvolution.y[2])**2+np.abs(ExcitedEvolution.y[3])**2,color = 'k')
 
-# ax[1].legend()
-# ax[1].set_xlabel('Time / ms')
-# ax[0].set_ylabel('State Population')
-# ax[1].set_ylabel('State Population')
-# ax[1].set_title(r'Initialization in 1st blue sideband $\vert 1,g\rangle$ - With recoil kick')
+ax[1].legend()
+ax[1].set_xlabel('Time / ms')
+ax[0].set_ylabel('State Population')
+ax[1].set_ylabel('State Population')
+ax[1].set_title(r'Initialization in 1st blue sideband $\vert 1,g\rangle$ - With recoil kick')
 
 
-# fig,ax = plt.subplots(2,1,figsize = (10,15))
-# ax[0].plot(UnExcitedEvolution.t/1e-3,np.abs(UnExcitedEvolution.y[0])**2+np.abs(UnExcitedEvolution.y[2])**2)
-# ax[1].plot(UnExcitedEvolution.t/1e-3,np.abs(ExcitedEvolution.y[0])**2+np.abs(ExcitedEvolution.y[2])**2)
-# ax[0].set_title(r'Initialization in ground state $\vert 0,g\rangle$  - No recoil kick')
-# ax[1].set_title(r'Initialization in 1st blue sideband $\vert 1,g\rangle$ - With recoil kick')
+fig,ax = plt.subplots(2,1,figsize = (10,15))
+ax[0].plot(UnExcitedEvolution.t/1e-3,np.abs(UnExcitedEvolution.y[0])**2+np.abs(UnExcitedEvolution.y[2])**2)
+ax[1].plot(UnExcitedEvolution.t/1e-3,np.abs(ExcitedEvolution.y[0])**2+np.abs(ExcitedEvolution.y[2])**2)
+ax[0].set_title(r'Initialization in ground state $\vert 0,g\rangle$  - No recoil kick')
+ax[1].set_title(r'Initialization in 1st blue sideband $\vert 1,g\rangle$ - With recoil kick')
 
-# for axe in ax:
-#     axe.set_ylim(0,1.05)
-#     axe.set_ylabel('Shelving probability')
-# for axe in ax:
-#     axe.axhline(1,ls = '--',color = 'k')
-# ax[1].set_xlabel('Time / ms')
+for axe in ax:
+    axe.set_ylim(0,1.05)
+    axe.set_ylabel('Shelving probability')
+for axe in ax:
+    axe.axhline(1,ls = '--',color = 'k')
+ax[1].set_xlabel('Time / ms')
 # # # eigvals,eigvecs = LA.eig(TI.Dicke_Hamilton(2*np.pi*6.61*1e3,-frequencies[2]-2*np.pi*0*1e3,frequencies[2],0,m_ba,vectors[2][0],k))
 # # # print('Eigenvalues are:',eigvals)
 # # # print('Eigenvectors are',eigvecs)
@@ -153,35 +153,35 @@ ax2twin.set_ylabel(r'$\eta_i^2$ for Porypherine with 550nm light',color = 'r')
 # # # # goodflop = gssmax(flopmax,-10,10,n_runs = 20)
 # # # # print(' Bonus detuning is ',goodflop,'kHz')
 # # # # # t_end_L = 1e-1
-# # def BigTimeEv(t,phi):
-# #     return 1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(2*np.pi*75*1e3,-frequencies[-1]+(2*np.pi*75*1e3)**2/(2*frequencies[-1]),frequencies[-1],t,m_ba,vectors[-1][0],k),phi)
-# # BigUnExcited = solve_ivp(BigTimeEv,[0,t_end],state,t_eval=np.linspace(0,t_end,10000),max_step = 1e-8)
-# # BigExcited = solve_ivp(BigTimeEv,[0,t_end],ExcState,t_eval=np.linspace(0,t_end,10000),max_step = 1e-8)
-# # fig,ax = plt.subplots(2,1,figsize = (10,15))
-# # fig.suptitle('12 Porypherin System')
-# # labels = [r'$\vert 0,e\rangle$',r'$\vert 0,g\rangle$',r'$\vert 1,e\rangle$',r'$\vert 1,g\rangle$']
-# # for j,y_s in enumerate(BigUnExcited.y):
-# #   ax[0].plot(BigUnExcited.t,np.abs(y_s)**2,label = labels[j])
-# # for j,y_s in enumerate(BigExcited.y):
-# #     ax[1].plot(BigExcited.t,np.abs(y_s)**2,label = labels[j])
-# # ax[0].plot(BigUnExcited.t,np.abs(BigUnExcited.y[0])**2+np.abs(BigUnExcited.y[1])**2+np.abs(BigUnExcited.y[2])**2+np.abs(BigUnExcited.y[3])**2,ls = '--',color = 'k')
-# # ax[1].plot(BigExcited.t,np.abs(BigExcited.y[0])**2+np.abs(BigExcited.y[1])**2+np.abs(BigExcited.y[2])**2+np.abs(BigExcited.y[3])**2,ls = '--',color = 'k')
-# # for axe in ax:
-# #     axe.legend()
-# # # def BigLongExcitation(t,phi):
-# # #      return 1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(6.5*1e3,-frequencies[-1],frequencies[-1],t,m_ba,vectors[-1][0],k),phi)
-# # # LongUnexcited = solve_ivp(BigLongExcitation,[0,0.01],state,t_eval = np.arange(0,0.01,1e-7))
-# # # LongExcited = solve_ivp(BigLongExcitation,[0,0.01],ExcState,t_eval = np.arange(0,0.01,1e-7))
+def BigTimeEv(t,phi):
+    return 1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(2*np.pi*75*1e3,-frequencies[-1]+(2*np.pi*75*1e3)**2/(2*frequencies[-1]),frequencies[-1],t,m_ba,vectors[-1][0],k),phi)
+BigUnExcited = solve_ivp(BigTimeEv,[0,t_end],state,t_eval=np.linspace(0,t_end,10000),max_step = 1e-8)
+BigExcited = solve_ivp(BigTimeEv,[0,t_end],ExcState,t_eval=np.linspace(0,t_end,10000),max_step = 1e-8)
+fig,ax = plt.subplots(2,1,figsize = (10,15))
+fig.suptitle('12 Porypherin System')
+labels = [r'$\vert 0,e\rangle$',r'$\vert 0,g\rangle$',r'$\vert 1,e\rangle$',r'$\vert 1,g\rangle$']
+for j,y_s in enumerate(BigUnExcited.y):
+  ax[0].plot(BigUnExcited.t,np.abs(y_s)**2,label = labels[j])
+for j,y_s in enumerate(BigExcited.y):
+    ax[1].plot(BigExcited.t,np.abs(y_s)**2,label = labels[j])
+ax[0].plot(BigUnExcited.t,np.abs(BigUnExcited.y[0])**2+np.abs(BigUnExcited.y[1])**2+np.abs(BigUnExcited.y[2])**2+np.abs(BigUnExcited.y[3])**2,ls = '--',color = 'k')
+ax[1].plot(BigExcited.t,np.abs(BigExcited.y[0])**2+np.abs(BigExcited.y[1])**2+np.abs(BigExcited.y[2])**2+np.abs(BigExcited.y[3])**2,ls = '--',color = 'k')
+for axe in ax:
+    axe.legend()
+def BigLongExcitation(t,phi):
+     return 1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(6.5*1e3,-frequencies[-1],frequencies[-1],t,m_ba,vectors[-1][0],k),phi)
+LongUnexcited = solve_ivp(BigLongExcitation,[0,0.01],state,t_eval = np.arange(0,0.01,1e-7))
+LongExcited = solve_ivp(BigLongExcitation,[0,0.01],ExcState,t_eval = np.arange(0,0.01,1e-7))
 
 
 
-# # # fig,ax = plt.subplots(2,1,figsize = (10,15))
-# # # for j,y_s in enumerate(LongUnexcited.y):
-# # #     ax[0].plot(LongUnexcited.t,np.abs(y_s)**2,label = labels[j])
-# # # for j,y_s in enumerate(LongExcited.y):
-# # #     ax[1].plot(LongExcited.t,np.abs(y_s)**2,label = labels[j])
-# # # for axe in ax:
-# # #     axe.legend()
+fig,ax = plt.subplots(2,1,figsize = (10,15))
+for j,y_s in enumerate(LongUnexcited.y):
+    ax[0].plot(LongUnexcited.t,np.abs(y_s)**2,label = labels[j])
+for j,y_s in enumerate(LongExcited.y):
+    ax[1].plot(LongExcited.t,np.abs(y_s)**2,label = labels[j])
+for axe in ax:
+    axe.legend()
 
 # # # print(1/(1j*hbar)*np.matmul(TI.Dicke_Hamilton(2*np.pi*6.5*1e3,-frequencies[-1],frequencies[-1],0,m_ba,vectors[-1][0],k),ExcState)*1e-8)
 # # # print(1/(1j*hbar)*TI.Dicke_Hamilton(2*np.pi*6.5*1e3,-frequencies[-1],frequencies[-1],0,m_ba,vectors[-1][0],k))
--- Two-Ion-Systems/DirectSideband_Different_Systems.py
+++ Two-Ion-Systems/DirectSideband_Different_Systems.py
@@ -1,3 +1,60 @@
 import numpy as np
 import TwoIons as TI
 import matplotlib.pyplot as plt
+plt.rc('font',size = 14)
+r0s = np.linspace(1,3.5,100)*1e-3
+m_ba = 135*1.66*1e-27
+charge_ba = 1*1.6*1e-19
+q_set = 0.4
+a_set = -0.01
+V_RF_our = (2*np.pi*5.2*1e6)**2*q_set*m_ba*(3/4*3.5*1e-3)**2/(2*charge_ba)
+V_DC_our = -a_set*m_ba*(3/4*2.7*1e-3)**2*(2*np.pi*5.2*1e6)**2/(4*charge_ba*0.248)
+print('Our RF Voltage is', V_RF_our,'Volts')
+print('Our DC Voltage is ', V_DC_our,'Volts')
+
+omega_rfs = np.sqrt((2*charge_ba*V_RF_our)/(m_ba*q_set*r0s**2))
+
+z0s = np.sqrt((-4*charge_ba*0.248*V_DC_our)/(m_ba*a_set*omega_rfs**2))
+a_check = (-4*charge_ba*0.248*V_DC_our)/(m_ba*z0s**2*omega_rfs**2)
+print('Checking a',a_check)
+q_check = (2*charge_ba*V_RF_our)/(m_ba*r0s**2*omega_rfs**2)
+print('q check',q_check)
+
+#We've checked that our system parameters are functioning. Now to make a bunch of systems.
+Traps = [TI.Trap(w_rf,0.248,z0,r0) for w_rf,z0,r0 in zip(omega_rfs,z0s,r0s)]
+m_por = 9000*1.66*1e-27
+charge_por = 24*1.6*1e-19
+
+Systems = [TI.two_ion_system(m_ba,charge_ba,m_por,charge_por,trap) for trap in Traps]
+w_s= [sys.vibrational_freqs_and_modes(a_set,q_set)[0] for sys in Systems]
+w_ins = np.array([w[0] for w in w_s])
+v_s = [sys.vibrational_freqs_and_modes(a_set,q_set)[1] for sys in Systems]
+v_ins = np.array([v[0] for v in v_s])
+def r0_to_rf_MHz_divide_2pi(r0):
+    rf = np.sqrt(2*charge_ba*V_RF_our/(m_ba*q_set*(r0*1e-3)**2))*1e-6/(2*np.pi)
+    return np.round(rf,2)
+alphas = np.array([1,2,3,4,5])
+fig,ax = plt.subplots(figsize = (10,15))
+for alpha in alphas:
+    ax.plot(r0s*1e3,w_ins/(2*np.pi*1e3*alpha),label = r'$\alpha = ${0}'.format(alpha))
+ax.legend()
+ax.set_xlabel(r'$r_0$ / mm')
+ax.set_ylabel(r'$\Omega_c$ / kHz')
+ax2 = ax.twiny()
+ax1ticks = ax.get_xticks()
+ax2ticks = ax1ticks
+ax2.set_xticks(ax2ticks)
+ax2.set_xticklabels(r0_to_rf_MHz_divide_2pi(ax2ticks))
+ax2.set_xbound(ax.get_xbound())
+ax2.set_xlabel('RF Frequency / MHz')
+fig.suptitle(r'Carrier Rabi frequency for a given $ \alpha = \frac{\delta}{\Omega_c}$, assuming 12 porypherin rings')
+
+
+
+fig,ax = plt.subplots(figsize = (10,15))
+for alpha in alphas:
+    dickes = np.array([TI.dicke_param(w,2*np.pi/(1762*1e-9),m_ba,v[0]) for w,v in zip(w_ins,v_ins)])
+    print(np.shape(dickes))
+    ax.plot(r0s*1e3,w_ins/(2*np.pi*1e3*alpha)*dickes)
+ax.set_ylim(0,1.8)
+plt.show()
